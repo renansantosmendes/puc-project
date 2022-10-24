b15e8ca5f095fac5b879ace67f27bce4b32d50e4
Renan Santos <renansantosmendes@gmail.com>
2022-10-23 23:44:42 -0300

    add model and scaler test

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pytest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
 from fastapi.testclient import TestClient
-from main import app
+from main import app, model, scaler
 
 client = TestClient(app)
 
@@ -8,3 +10,11 @@ client = TestClient(app)
 def test_home():
     response = client.get('/')
     assert response.json() == {"Hello": "World"}
+
+
+def test_model():
+    assert isinstance(model, RandomForestClassifier)
+
+
+def test_scaler():
+    assert isinstance(scaler, StandardScaler)
